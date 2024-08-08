bd80dc36527f9e48b77d6144ef5fa5a577afbeb6
Mohammed Essa <moessa089@gmail.com>
2024-08-08 18:44:15 +0000

    Added functionality to upload pictures and create new tags

--- album/urls.py
+++ album/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePage, name='home'),
     path('welcome/', views.WelcomePage, name='welcome'),
-    path('photo/<int:id>/', views.viewPhoto, name='photo')
+    path('photo/<int:id>/', views.viewPhoto, name='photo'),
+    path('add/', views.addPhoto, name='add')
 ]
--- album/views.py
+++ album/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
-from .models import Tag, Photo 
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Tag, Photo, Profile 
 
-# Create your views here.
-# class HomePage(TemplateView):
-#     """
-#     Displays home page'
-#     """
-#     template_name = 'index.html'
+
 
 def HomePage(request):
     return render(request, 'album/index.html')
@@ -22,3 +17,40 @@ def WelcomePage(request):
 def viewPhoto(request, id):
     photo = get_object_or_404(Photo, id=id)
     return render(request, 'album/photo.html', {'photo': photo})
+
+@login_required
+def addPhoto(request):
+    tags = Tag.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        selected_tag_id = data.get('tags', 'none')
+        new_tag_name = data.get('tags_new', '')
+
+        tags_to_add = []
+
+        if selected_tag_id != 'none':
+            selected_tag = Tag.objects.get(id=selected_tag_id)
+            tags_to_add.append(selected_tag)
+        elif new_tag_name != '':
+            new_tag, created = Tag.objects.get_or_create(name=new_tag_name)
+            tags_to_add.append(new_tag)
+
+        # Get the profile of the logged-in user
+        profile = get_object_or_404(Profile, user=request.user)
+
+        if image:
+            photo = Photo.objects.create(
+                profile=profile,
+                caption=data.get('caption', ''),
+                image=image,
+            )
+
+            if tags_to_add:
+                photo.tags.set(tags_to_add)  # Add tags to the photo
+
+        return redirect('welcome')
+
+    context = {'tags':tags}
+    return render(request, 'album/add_photo.html', context)
