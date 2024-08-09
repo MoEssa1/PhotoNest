ba3705de419ce2e6990a420d2352814740cdcd1f
Mohammed Essa <moessa089@gmail.com>
2024-08-09 14:53:42 +0000

    Created signals.py file to auto create profiles to go along user models

--- album/apps.py
+++ album/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AlbumConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'album'
+
+    # this will ensure the signals.py file gets imported
+    def ready(self):
+        import album.signals 
