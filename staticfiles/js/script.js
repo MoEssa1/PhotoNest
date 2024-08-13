1aaee5992b32a06513db456493d4af07d27931e5
Mohammed Essa <moessa089@gmail.com>
2024-08-13 11:41:48 +0000

    Added script.js for alert timer, resized elements for smaller screen

---
+++ staticfiles/js/script.js
@@ -0,0 +1,6 @@
+// Timer for alert messages
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
