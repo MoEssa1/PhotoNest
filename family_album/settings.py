0219f6f03eb6353904e37369d39661a13705707b
Mohammed Essa <moessa089@gmail.com>
2024-08-14 13:24:10 +0000

    Fixed bug in label for att on update_photo.html

--- family_album/settings.py
+++ family_album/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-moessa1-photonest-mtfna3zn5fv.ws.codeinstitute-ide.net',
